d0833411a28e43567b0c36bdbb82a3abdfff8379
abbrenner-CBHC <80916607+abbrenner-CBHC@users.noreply.github.com>
2021-05-05 13:35:28 -0700

    Addeded data import for Prescott and some data management to graph some of the data

--- raw_data/import-data.R
+++ raw_data/import-data.R
@@ -19,6 +19,11 @@ ed_cbhc_tyd <- read_xlsx("raw_data/credible_for_ed_ytd_04_28_2020.xlsx",
               clean_names()
 
 
+prescott_march_wide <- read_excel(path= "clean_data/ed-prescott-march-wide.xls",
+                                  sheet="Sheet1",
+                                  col_names = TRUE) %>% 
+  clean_names()
+
 
 # Basic data cleaning, emergency department encounters, YTD -----------------------------------------------------
 
@@ -69,7 +74,7 @@ ed_encounter_dates<- ed_encounter_clean %>%
 # Create count of ED admits for each person
 ed_encounter_dates %>% 
   group_by(id) %>% 
-  mutate(day_admit_count = (add_count(day_admit)))
+  add_count(day_admit, name = client_ed_visit_total)
 
 # Create high utilization categories
 ed_encounter_dates <- ed_encounter_dates %>% 
@@ -97,3 +102,28 @@ ed_person_summary_data >- ed_encounter_clean %>%
   select()
 
 
+## PRESCOTT - PLOTTING
+
+prescott_dx <- prescott_march_wide %>% 
+  select(member_id,primary_dx_desc, past_12_mo_5_9, past_12_mo_10_plus, past_month_ed_2_plus, bh_dm) %>%
+  filter(primary_dx_desc=="Reaction to severe stress, and adjustment disorders"|primary_dx_desc=="Schizoaffective disorders"|primary_dx_desc=="Unspecified psychosis not due to a substance or known physiological condition
+"|primary_dx_desc=="Recurrent depressive disorder"|primary_dx_desc=="Schizophrenia"|primary_dx_desc=="Bipolar affective disorder") 
+
+
+# I am trying to graph a count of clients by each of the three ed utilization groups (2+ times in past month,
+# 5-9 times in past year, 10+ times in past year, those with behavioral health Dx and diabetes)
+# I thought if I used pivot_longer to create a ed type variable, and then used facet wrap to show the count by diagnosis for each
+# utilization category, it might give me a nice way to compare. But it's not working and I'm not sure where to
+# go from here
+
+
+prescott_dx_long <- prescott_dx %>% 
+  pivot_longer(cols = !(c("member_id","primary_dx_desc")),
+               names_to = "ed_visit_type", "percent_of_clients") %>% 
+  rename("flag"="value") %>% 
+  ggplot(mapping=aes(x="primary_dx_desc",
+                     y=flag)) +
+  geom_col()+
+  labs(y="Number of Clients",
+       x="Diagnosis")+
+  facet_wrap(~vars(ed_visit_type))
